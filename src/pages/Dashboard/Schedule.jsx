52f4a7f96e15b109cd42df471f52be2e5336f88b
mahatav6 <mahatabcall56@gmail.com>
2025-08-30 21:41:05 +0600

    modal close btn added

--- src/pages/Dashboard/Schedule.jsx
+++ src/pages/Dashboard/Schedule.jsx
@@ -31,7 +31,7 @@ export default function Schedule() {
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn" onClick={closeModal}>Close</button>
+              <button className="btn">Close</button>
             </form>
           </div>
         </div>
